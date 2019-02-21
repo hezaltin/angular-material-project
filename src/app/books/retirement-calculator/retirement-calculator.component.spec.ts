f463d446feb2a09c02237b1c7ce305f79a09db0f
DESKTOP-50P9098\Michael Hezaltin <hezaltin@gmail.com>
2019-02-21 09:01:08 +0530

    calc-new-value-changes

---
+++ src/app/books/retirement-calculator/retirement-calculator.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RetirementCalculatorComponent } from './retirement-calculator.component';
+
+describe('RetirementCalculatorComponent', () => {
+  let component: RetirementCalculatorComponent;
+  let fixture: ComponentFixture<RetirementCalculatorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RetirementCalculatorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RetirementCalculatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/books/retirement-calculator/retirement-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-retirement-calculator',
+  templateUrl: './retirement-calculator.component.html',
+  styleUrls: ['./retirement-calculator.component.css']
+})
+export class RetirementCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
